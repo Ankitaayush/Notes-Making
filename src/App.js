30fce5d2755b6d53f67907f6ee6fdae73804ec8f
Ankitaayush <aayushkumar152001@gmail.com>
2022-02-04 21:14:47 +0530

    note making

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import {useState,useEffect}from 'react'
+import {nanoid} from 'nanoid'
+import NotesList from './componenets/NotesList'
+import Search from './componenets/Search'
+import Header from './componenets/Header'
+const App =() => {
+  const[notes,setNotes]=useState([{
+    id:nanoid(),
+  text:"This is ",
+  date:"3/02/2022"
+},
+{
+  id:nanoid(),
+text:"This is ",
+date:"3/02/2022"
+},{
+  id:nanoid(),
+text:"This is ",
+date:"3/02/2022"
+},{
+  id:nanoid(),
+text:"This is ",
+date:"3/02/2022"
+}])
+
+
+
+const [searchText, setSearchText]= useState('');
+const [darkMode,setDarkMode]=useState(false)
+
+
+useEffect(()=>{
+const savedNotes=JSON.parse(localStorage.getItem('react-notes-app-data'))
+if(savedNotes){
+  setNotes(savedNotes);
+}
+},[])
+useEffect(() => {
+  localStorage.setItem('react-notes-app-data',JSON.stringify(notes))
+  
+}, [notes]);
+
+const addNote=(text)=>{
+  const date=new Date();
+  const newNote={
+    id:nanoid(),
+    text: text,
+    date:date.toLocaleDateString()
+  }
+  const newNotes=[...notes,newNote];
+  setNotes(newNotes)
+
+
+};
+
+const deleteNode = (id)=>{
+  const newNotes=notes.filter ((note) =>note.id !==id)
+  setNotes(newNotes)
+
+}
+  return(
+    <div className={`${darkMode && 'dark-mode'}`}>
+  <div className="container ">
+
+    <Header handleToggleDarkMode={setDarkMode}/>
+    <Search handleSearchNote={setSearchText}/>
+    <NotesList
+        notes ={notes.filter((note)=>
+  note.text.toLowerCase().includes(searchText)
+  )}
+     
+     handleAddNote={addNote}
+     handleDeleteNote={deleteNode}/>
+  </div>
+  </div>
+  );
+}
+export default App 
